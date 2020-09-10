3ac6a7522748ff85e05f679ad59074932b2dba28
Michail Smith <misha@gomerits.com>
2020-09-09 17:46:03 -0700

    Remove hardcoded localhost from environment.ts

--- frontend/src/relay/environment.ts
+++ frontend/src/relay/environment.ts
@@ -3,7 +3,7 @@ import { Environment, Network, Store, RecordSource } from "relay-runtime";
 export const environment = new Environment({
   store: new Store(new RecordSource()),
   network: Network.create((operation, variables, cacheConfig, uploadables) => {
-    return fetch("http://localhost:8080/query", {
+    return fetch("/query", {
       method: "POST",
       headers: {
         // Add authentication and other headers here
